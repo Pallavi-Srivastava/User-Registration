383038e6d7ec699d63babbe1a53f86105286153a
Pallavi-Srivastava <69475236+Pallavi-Srivastava@users.noreply.github.com>
2020-10-24 22:24:52 +0530

    Modified: Remove Extra Add Block

--- src/main/java/com/blz/userregistration/UserRegistration.java
+++ src/main/java/com/blz/userregistration/UserRegistration.java
@@ -3,12 +3,19 @@ package com.blz.userregistration;
 public class UserRegistration {
 	public static void main(String args[]) {
 		EntryUserRegistration ur = new EntryUserRegistration();
-		/*
-		 * // First name validation ur.isValidFirstName(); // Second name validation
-		 * ur.isValidSecondName(); // Email validation ur.isValidEmail(); // MobileNo
-		 * validation ur.isValidMobileNo(); // Password validation a/c to rule 1
-		 * ur.isValidPasswordAccToRule1(); // Password validation a/c to rule 2
-		 * ur.isValidPasswordAccToRule2(); // Password validation a/c to rule 3
-		 */		ur.isValidPasswordAccToRule3();
+		// First name validation
+		ur.isValidFirstName();
+		// Second name validation
+		ur.isValidSecondName();
+		// Email validation
+		ur.isValidEmail();
+		// MobileNo
+		ur.isValidMobileNo();
+		// Password validation a/c to rule1
+		ur.isValidPasswordAccToRule1();
+		// Password validation a/c to rule2
+		ur.isValidPasswordAccToRule2();
+		// Password validation a/c to rule3
+		ur.isValidPasswordAccToRule3();	
 	}
 }
