5e897c7d8badbae734f28f26015326f0bcf464e6
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-10-24 19:24:20 +0530

    Added:Validation of user last name

---
+++ src/main/java/com/blz/userregistration/EntryUserRegistration.java
@@ -0,0 +1,32 @@
+package com.blz.userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EntryUserRegistration {
+	// Method for first name validation
+	public void isValidFirstName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your First name : ");
+		String fname = sc.nextLine();
+		String regex = "^[A-Z]{1}[a-z]{2,}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(fname);
+		boolean match = matcher.matches();
+		System.out.println("First name : " + fname + " is " + match);
+	}
+
+	// Method for second name validation
+	public void isValidSecondName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your Last name : ");
+		String lname = sc.nextLine();
+		String regex = "^[A-Z]{1}[a-z]{2,}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lname);
+		boolean match = matcher.matches();
+		System.out.println("Last name : " + lname + " is " + match);
+	}
+
+}
---
+++ src/main/java/com/blz/userregistration/UserRegistration.java
@@ -0,0 +1,11 @@
+package com.blz.userregistration;
+
+public class UserRegistration {
+	public static void main(String args[]) {
+		EntryUserRegistration ur = new EntryUserRegistration();
+		// First name validation
+		ur.isValidFirstName();
+		// Second name validation
+		ur.isValidSecondName();
+	}
+}
