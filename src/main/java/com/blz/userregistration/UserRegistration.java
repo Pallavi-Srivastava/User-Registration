ff3c0991d2715d423f3b78ed9f653da7628c2cf6
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-10-24 22:49:06 +0530

    Added:Validation for sample emails

---
+++ src/main/java/com/blz/userregistration/UserRegistration.java
@@ -0,0 +1,27 @@
+package com.blz.userregistration;
+
+public class UserRegistration {
+	public static void main(String args[]) {
+		EntryUserRegistration ur = new EntryUserRegistration();
+
+		// First name validation
+		ur.isValidFirstName();
+		// Second name validation
+		ur.isValidSecondName();
+		// Email validation
+		ur.isValidEmail();
+		// MobileNo
+		ur.isValidMobileNo();
+		// Password validation a/c to rule1
+		ur.isValidPasswordAccToRule1();
+		// Password validation a/c to rule2
+		ur.isValidPasswordAccToRule2();
+		// Password validation a/c to rule3
+		ur.isValidPasswordAccToRule3();
+		// Password validation a/c to rule4
+		ur.isValidPasswordAccToRule4();
+		// Sample Email validation
+		ur.isValidSampleEmail();
+
+	}
+}
