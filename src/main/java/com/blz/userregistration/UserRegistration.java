91d424232cbabc467353da9852745eca0753631f
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-10-24 21:14:04 +0530

    Added:Validate user MobileNo

---
+++ src/main/java/com/blz/userregistration/UserRegistration.java
@@ -0,0 +1,15 @@
+package com.blz.userregistration;
+
+public class UserRegistration {
+	public static void main(String args[]) {
+		EntryUserRegistration ur = new EntryUserRegistration();
+		// First name validation
+		ur.isValidFirstName();
+		// Second name validation
+		ur.isValidSecondName();
+		// Email validation
+		ur.isValidEmail();
+		// MobileNo validation
+		ur.isValidMobileNo();
+	}
+}
