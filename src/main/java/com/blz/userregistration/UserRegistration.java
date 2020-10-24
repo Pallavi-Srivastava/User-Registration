ae4bf6abc364c136ebd23fb4d0c73c3d18935062
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-10-24 19:36:54 +0530

    Added:Validation of user email

---
+++ src/main/java/com/blz/userregistration/UserRegistration.java
@@ -0,0 +1,13 @@
+package com.blz.userregistration;
+
+public class UserRegistration {
+	public static void main(String args[]) {
+		EntryUserRegistration ur = new EntryUserRegistration();
+		// First name validation
+		ur.isValidFirstName();
+		// Second name validation
+		ur.isValidSecondName();
+		// Email validation
+		ur.isValidEmail();
+	}
+}
