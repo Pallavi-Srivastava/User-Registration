05552f18ae400de09a3cc1888940c3f15812aa67
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-10-24 21:55:34 +0530

    Added:Validation for user Password According to Rule3

---
+++ src/main/java/com/blz/userregistration/UserRegistration.java
@@ -0,0 +1,14 @@
+package com.blz.userregistration;
+
+public class UserRegistration {
+	public static void main(String args[]) {
+		EntryUserRegistration ur = new EntryUserRegistration();
+		/*
+		 * // First name validation ur.isValidFirstName(); // Second name validation
+		 * ur.isValidSecondName(); // Email validation ur.isValidEmail(); // MobileNo
+		 * validation ur.isValidMobileNo(); // Password validation a/c to rule 1
+		 * ur.isValidPasswordAccToRule1(); // Password validation a/c to rule 2
+		 * ur.isValidPasswordAccToRule2(); // Password validation a/c to rule 3
+		 */		ur.isValidPasswordAccToRule3();
+	}
+}
